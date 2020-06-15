3ccfb26400d9b19021236f907517324260086f4c
JaehunKim-DeepLearning <45785355+JaehunKim-DeepLearning@users.noreply.github.com>
2020-06-15 13:35:22 +0900

    Update train_start.sh

--- train_start.sh
+++ train_start.sh
@@ -17,7 +17,7 @@ python3 feature_extraction.py $SONYC_UST_PATH/data/audio/ $SONYC_UST_PATH/embedd
 python3 classify.py $SONYC_UST_PATH/data/annotations_train.csv $SONYC_UST_PATH/data/dcase-ust-taxonomy.yaml $SONYC_UST_PATH/output baseline_fine \
   --label_mode fine --learning_rate 1e-4 --l2_reg 1e-4 \
   --dropout_size 0.4 --ef_mode 4 --num_epochs 8 \
-  --emb_dir /home/server/Dcase/task5/embeddings_ef_3ch_5s_train/
+  --emb_dir SONYC_UST_PATH/embeddings_ef_3ch_5s_train/
 
 python3 evaluate_predictions.py $SONYC_UST_PATH/output/baseline_fine/ $SONYC_UST_PATH/data/annotations_train.csv $SONYC_UST_PATH/data/dcase-ust-taxonomy.yaml
 
