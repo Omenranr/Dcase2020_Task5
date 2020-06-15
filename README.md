b6ee37074e4af081699080ea0d0789b337bd957b
JaehunKim-DeepLearning <45785355+JaehunKim-DeepLearning@users.noreply.github.com>
2020-06-15 11:40:51 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Dcase2020_Task5
-Dcase2020_Task5
+# DCASE 2020 Challenge: Task 5 - JHKim
+
+Must be located the path(data/audio) the train and validate data
+
+train_start.sh --> Project Sequence shell
+
+src/data_create.py --> Prepare the feature for train
+
+src/classify.py --> Train, create Model and predict validate set
+
+src/evaluate_predictions.py --> Evaluate the predicted validate set
+
+proposed_evaluation_output.json --> Best result of task5 in our model
---
+++ train_start.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Activate environment
+#source activate sonyc-ust
+
+# Extract embeddings
+
+#pip3 install "tensorflow-gpu==2.0.0"
+#pip3 install "tensorflow-gpu<1.14"
+
+SONYC_UST_PATH=/home/server/바탕화면/소스코드/연구코드/Dcase/task5
+
+pushd src
+
+python3 data_create.py $SONYC_UST_PATH/data/audio/ $SONYC_UST_PATH/embeddings_ef_3ch_5s_train/
+
+python3 classify.py $SONYC_UST_PATH/data/annotations_train.csv $SONYC_UST_PATH/data/dcase-ust-taxonomy.yaml $SONYC_UST_PATH/output baseline_fine \
+  --label_mode fine --learning_rate 1e-4 --l2_reg 1e-4 \
+  --dropout_size 0.4 --ef_mode 4 --num_epochs 8 \
+  --emb_dir /home/server/바탕화면/소스코드/연구코드/Dcase/task5/embeddings_ef_3ch_5s_train/
+
+python3 evaluate_predictions.py $SONYC_UST_PATH/output/baseline_fine/ $SONYC_UST_PATH/data/annotations_train.csv $SONYC_UST_PATH/data/dcase-ust-taxonomy.yaml
+
+popd
