a9e1bb60c8e31702ffa8e09981138105b78006f9
JaehunKim-DeepLearning <45785355+JaehunKim-DeepLearning@users.noreply.github.com>
2020-06-15 13:52:10 +0900

    Update train_start.sh

--- train_start.sh
+++ train_start.sh
@@ -2,22 +2,6 @@
 
 SONYC_UST_PATH=/home/server/Dcase/task5 ### Project path
 
-yes | pip3 install -r requirements.txt
-
-# Download dataset
-mkdir -p $SONYC_UST_PATH/embeddings_ef_3ch_5s_train
-mkdir -p $SONYC_UST_PATH/data
-pushd $SONYC_UST_PATH/data
-wget https://zenodo.org/record/3693077/files/audio-dev.tar.gz
-wget https://zenodo.org/record/3693077/files/dcase-ust-taxonomy.yaml
-wget https://zenodo.org/record/3693077/files/README.md
-
-# Decompress audio
-tar xf audio.tar.gz
-rm audio.tar.gz
-popd
-
-
 pushd src
 
 # Feature Extration
