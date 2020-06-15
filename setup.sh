8ac23a0c08d15e243dba0a710c8ccff7ad4e81a2
JaehunKim-DeepLearning <45785355+JaehunKim-DeepLearning@users.noreply.github.com>
2020-06-15 13:44:48 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 # Create environment
-conda create -n sonyc-ust-stc python=3.6 -y
-source activate sonyc-ust-stc
+#conda create -n sonyc-ust-stc python=3.6 -y
+#source activate sonyc-ust-stc
 
 # Install dependencies
-yes | pip install -r requirements.txt
+yes | pip3 install -r requirements.txt
 
 # Download dataset
 mkdir -p $SONYC_UST_PATH/data
 pushd $SONYC_UST_PATH/data
-wget https://zenodo.org/record/3693077/files/annotations.csv
+#wget https://zenodo.org/record/3693077/files/annotations.csv
 wget https://zenodo.org/record/3693077/files/audio-dev.tar.gz
 wget https://zenodo.org/record/3693077/files/dcase-ust-taxonomy.yaml
 wget https://zenodo.org/record/3693077/files/README.md
