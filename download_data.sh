bcabca21bc545f1ee89cadd8548c6f3d5a5af035
JaehunKim-DeepLearning <45785355+JaehunKim-DeepLearning@users.noreply.github.com>
2020-06-15 13:51:53 +0900

    Create download_data.sh

---
+++ download_data.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+SONYC_UST_PATH=/home/server/Dcase/task5 ### Project path
+
+yes | pip3 install -r requirements.txt
+
+# Download dataset
+mkdir -p $SONYC_UST_PATH/embeddings_ef_3ch_5s_train
+mkdir -p $SONYC_UST_PATH/data
+pushd $SONYC_UST_PATH/data
+wget https://zenodo.org/record/3693077/files/audio-dev.tar.gz
+wget https://zenodo.org/record/3693077/files/dcase-ust-taxonomy.yaml
+wget https://zenodo.org/record/3693077/files/README.md
+
+# Decompress audio
+tar xf audio.tar.gz
+rm audio.tar.gz
+popd
